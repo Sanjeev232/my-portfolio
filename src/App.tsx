1c380d431ac3c3b33fbca4ea149f0cad8ea1a5eb
Sanjeev Singh Negi <sanjeev@kommunicate.io>
2024-02-12 06:10:46 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import NavBar from "./View/NavBar";
+import Home from "./View/Home";
+// import About from "./View/About";
+// import Experience from "./View/Experience";
+import Footer from "./View/Footer";
+import Contact from "./View/Contact";
 
 function App() {
   return (
     <>
       <NavBar />
-    </>
+      <Home />
+      {/* <About /> */}
+      {/* <Experience /> */}
+      <Contact />
+      <Footer />
+    </div>
   );
 }
 
