b6ca6aea1bea745b2f100553608f2ae6f7868787
Sanjeev232 <contactsanjeev.singh@yahoo.com>
2025-04-10 23:53:20 +0530

    removeunused variable

--- src/View/AboutMe/About.tsx
+++ src/View/AboutMe/About.tsx
@@ -18,7 +18,7 @@ interface Company {
 
 const About = () => {
 	const currentWorkingCompany = getLastWorkedCompany(COMPANY_WORKED) as Company;
-	const { name, designation, location, link } = currentWorkingCompany;
+	const { name, designation, location } = currentWorkingCompany;
 	const {
 		name: firstCompanyName,
 		startedAt: firstCompanyStartedAt,
